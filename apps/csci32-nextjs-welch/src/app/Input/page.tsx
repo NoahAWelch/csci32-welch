2cb5dc3f7790b61351a0815ff58879812d1e584f
NoahAWelch <148398477+NoahAWelch@users.noreply.github.com>
2024-09-17 16:52:16 -0700

    Input Lab (#1)

---
+++ apps/csci32-nextjs-welch/src/app/Input/page.tsx
@@ -0,0 +1,89 @@
+'use client'
+import Input from '@repo/ui/input'
+import { Button } from '../../../../../packages/ui/src/button'
+import { Sizes } from '../../../../../packages/ui/src/size'
+import { Variants } from '../../../../../packages/ui/src/variant'
+import { useState } from 'react'
+import Navbar from '../Components/Navbar'
+
+export default function ButtonProps() {
+  const [name, setName] = useState('')
+  const [date, setDate] = useState('')
+  const [rating, setRating] = useState('')
+  const [review, setReview] = useState('')
+  return (
+    <div className="p-12 bg-gray-800">
+      <Navbar />
+      <div className="flex gap-2 flex-wrap flex-col min-w-screen min-h-screen border border-gray-200 rounded-lg shadow dark:bg-gray-500 dark:border-gray-700">
+        <div className="flex gap-2 justify-center py-12">
+          <Input
+            value={name}
+            setValue={setName}
+            size={Sizes.Medium}
+            variant={Variants.Primary}
+            name="name"
+            id="number"
+          />
+          <Button onClick={() => alert(`Enter your name: ${name}`)} size={Sizes.Medium} variant={Variants.Primary}>
+            {' '}
+            Name
+          </Button>
+        </div>
+        <div className="flex gap-2 justify-center py-12">
+          <Input
+            value={date}
+            setValue={setDate}
+            size={Sizes.Medium}
+            variant={Variants.Secondary}
+            name="name"
+            id="number"
+          />
+          <Button onClick={() => alert(`Date you read book: ${date}`)} size={Sizes.Medium} variant={Variants.Secondary}>
+            {' '}
+            Date
+          </Button>
+        </div>
+
+        <div className="flex gap-2 justify-center py-12">
+          <Input
+            value={rating}
+            setValue={setRating}
+            size={Sizes.Medium}
+            variant={Variants.Tertiary}
+            name="name"
+            id="number"
+          />
+          <Button
+            onClick={() => alert(`Rating for your book: ${rating}`)}
+            size={Sizes.Medium}
+            variant={Variants.Tertiary}
+          >
+            {' '}
+            Rating
+          </Button>
+        </div>
+        <div className="flex gap-2 justify-center py-12">
+          <Input
+            value={review}
+            setValue={setReview}
+            size={Sizes.Medium}
+            variant={Variants.TertiaryReview}
+            name="name"
+            id="number"
+          />
+
+          <div className="min-h-48">
+            <Button
+              onClick={() => alert(`Rating for your book: ${review}`)}
+              size={Sizes.Medium}
+              variant={Variants.TertiaryReview}
+            >
+              {' '}
+              Review
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- packages/ui/src/variant.ts
+++ packages/ui/src/variant.ts
@@ -2,4 +2,54 @@ export enum Variants {
   Primary = 'Primary',
   Secondary = 'Secondary',
   Tertiary = 'Tertiary',
+  TertiaryReview = 'TertiaryReview',
+}
+
+export function getVariant(variant: Variants) {
+  switch (variant) {
+    case Variants.Primary:
+      return 'bg-blue-600 hover:bg-blue-800 active:bg-blue-900'
+    case Variants.Secondary:
+      return 'bg-green-600 hover:bg-green-800 active:bg-green-900'
+    case Variants.Tertiary:
+      return 'bg-red-600 hover:bg-red-800 active:bg-red-900'
+    case Variants.TertiaryReview:
+      return 'bg-purple-600 hover:bg-purple-800 active:bg-purple-900'
+  }
+}
+export function getVariantOutlineStyles(variant: Variants) {
+  switch (variant) {
+    case Variants.Primary:
+      return 'outline-red-600'
+    case Variants.Secondary:
+      return 'outline-yellow-500'
+    case Variants.Tertiary:
+      return 'outline-blue-600'
+    case Variants.TertiaryReview:
+      return 'outline-pink-600'
+  }
+}
+export function getVariantBorderStyles(variant: Variants) {
+  switch (variant) {
+    case Variants.Primary:
+      return ' border-2 border-yellow-600'
+    case Variants.Secondary:
+      return 'border-2 border-blue-500'
+    case Variants.Tertiary:
+      return 'border-2 border-green-600'
+    case Variants.TertiaryReview:
+      return 'border-2 border-gray-600'
+  }
+}
+export function getVariantTextStyles(variant: Variants) {
+  switch (variant) {
+    case Variants.Primary:
+      return 'text-black font-bold'
+    case Variants.Secondary:
+      return 'text-black font-bold '
+    case Variants.Tertiary:
+      return 'text-black font-bold'
+    case Variants.TertiaryReview:
+      return 'text-black font-bold'
+  }
 }
