5baf93fa3bfadfd8844e610502c6679e071b2797
noah welch <noahwelch13@yahoo.com>
2024-09-17 17:27:25 -0700

    input lab fixes

--- apps/csci32-nextjs-welch/src/app/layout.tsx
+++ apps/csci32-nextjs-welch/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import '../../../../packages/ui/src/styles'
+import '../../../../packages/ui/src/styles.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
