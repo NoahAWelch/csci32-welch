fa6e18940915ef2073cd141cddfb03cede6468fe
noah welch <noahwelch13@yahoo.com>
2024-10-13 15:52:13 -0700

    Add recipestacker-api2 and related files

---
+++ apps/recipestacker-api2/src/routes/recipes.ts
@@ -0,0 +1,196 @@
+import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
+import Fastify from 'fastify'
+import { Type } from '@sinclair/typebox'
+import { FastifyPluginAsync } from 'fastify'
+
+export const UpsertIngredientMeasurmentTypeboxType = Type.Object({
+  unit: Type.Optional(Type.String()),
+  quantity: Type.Optional(Type.Number()),
+  ingredient_id: Type.Optional(Type.String()),
+  ingredient_name: Type.Optional(Type.String()),
+  ingredient_description: Type.Optional(Type.String()),
+})
+
+export const UpdateRecipeTypeBoxType = Type.Object({
+  name: Type.Optional(Type.String()),
+  description: Type.Optional(Type.String()),
+  ingredient_measurments: Type.Optional(Type.Array(UpsertIngredientMeasurmentTypeboxType)),
+})
+
+export const CreateRecipeTypeboxType = Type.Object({
+  name: Type.String(),
+  description: Type.String(),
+  ingredient_measurments: Type.Array(UpsertIngredientMeasurmentTypeboxType),
+})
+
+export const IngredientMeasurmentTypeboxType = Type.Object({
+  unit: Type.Optional(Type.String()),
+  quantity: Type.Optional(Type.Number()),
+  ingredient: Type.Object({
+    ingredient_id: Type.Optional(Type.String()),
+    name: Type.Optional(Type.Union([Type.String(), Type.Null()])),
+    description: Type.Optional(Type.Union([Type.String(), Type.Null()])),
+  }),
+})
+
+export const RecipeType = Type.Object({
+  recipe_id: Type.String(),
+  name: Type.Union([Type.String(), Type.Null()]),
+  ingredient_measurments: Type.Array(IngredientMeasurmentTypeboxType),
+  description: Type.String(),
+  user_id: Type.String(),
+})
+
+export const RecipeNotFoundType = Type.Object({
+  statusCode: Type.Literal(404),
+  message: Type.String(),
+  error: Type.Literal('Not Found'),
+})
+
+const recipe: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  fastify.withTypeProvider<TypeBoxTypeProvider>().get(
+    '/recipes',
+    {
+      schema: {
+        tags: ['Endpoint: Get recipes'],
+        description: 'Endpoint to get multiple recipes',
+        querystring: Type.Object({
+          name: Type.Optional(Type.String()),
+          sortColumn: Type.Optional(Type.Any()),
+          sortOrder: Type.Optional(Type.Any()),
+          take: Type.Optional(Type.Number()),
+          skip: Type.Optional(Type.Number()),
+        }),
+        response: {
+          200: Type.Array(RecipeType),
+          404: RecipeNotFoundType,
+        },
+      },
+    },
+    async function (request, reply) {
+      const recipes = await fastify.recipeService.findManyRecipes({
+        name: request.query.name,
+        sortColumn: request.query.sortColumn,
+        sortOrder: request.query.sortOrder,
+        take: request.query.take,
+        skip: request.query.skip,
+      })
+      return recipes
+    },
+  )
+
+  fastify.withTypeProvider<TypeBoxTypeProvider>().put(
+    '/recipes/:id',
+    {
+      schema: {
+        tags: ['Endpoint: Update a recipe'],
+        description: 'Endpoint to update one recipe',
+        body: UpdateRecipeTypeBoxType,
+        response: {
+          200: Type.Object({ recipe_id: Type.String() }),
+          400: Type.Object({ message: Type.String() }),
+        },
+      },
+    },
+    async function (request: any, reply) {
+      return fastify.recipeService.updateOneRecipe({
+        recipe_id: request.params.id,
+        name: request.body.name,
+        description: request.body.description,
+        ingredient_measurments: request.body.ingredient_measurments,
+      })
+    },
+  )
+
+  fastify.withTypeProvider<TypeBoxTypeProvider>().get(
+    '/recipes/:id',
+    {
+      schema: {
+        tags: ['Endpoint: Get one recipe'],
+        description: 'Endpoint to get one recipe',
+        params: Type.Object({
+          id: Type.String(),
+        }),
+        responce: {
+          200: RecipeType,
+          404: RecipeNotFoundType,
+        },
+      },
+    },
+    async function (request: any, reply) {
+      const recipe = await fastify.recipeService.findOneRecipe({
+        recipe_id: request.params.id,
+      })
+      return recipe
+    },
+  )
+
+  fastify.withTypeProvider<TypeBoxTypeProvider>().post(
+    '/recipes',
+    {
+      schema: {
+        tags: ['create a recipe'],
+        description: 'Endpoiint to create a recipe',
+        body: CreateRecipeTypeboxType,
+        response: {
+          200: Type.Object({ recipe_id: Type.String() }),
+          400: Type.Object({ message: Type.String() }),
+        },
+      },
+    },
+    async function (request: any, reply) {
+      return fastify.recipeService.createOneRecipe({
+        name: request.body.name,
+        description: request.body.description,
+        ingredient_measurments: request.body.ingredient_measurments,
+      })
+    },
+  )
+}
+export default recipe
+
+// "dev": "export NODE_ENV='development' && tsx watch src/app.ts"
+
+/* fastify.withTypeProvider<TypeBoxTypeProvider>().get(
+    '/recipes/:id',
+    {
+      schema: {
+        tags: ['Endpoint: Get one recipe'],
+        description: 'Endpoint to get one recipe',
+        body: UpdateRecipeTypeBoxType,
+        responce: {
+          200: RecipeType,
+          404: RecipeNotFoundType,
+        },
+      },
+    },
+    async function (request: any, reply) {
+      return fastify.recipeService.findOneRecipe({
+        recipe_id: request.params.id,
+      })
+    },
+  )*/
+
+/* fastify.withTypeProvider<TypeBoxTypeProvider>().get(
+    '/recipes',
+    {
+      schema: {
+        tags: ['Endpoint: Get one recipe'],
+        description: 'Endpoint to get one recipe',
+        body: UpdateRecipeTypeBoxType,
+        responce: {
+          200: Type.Array(RecipeType),
+          404: RecipeNotFoundType,
+        },
+      },
+    },
+    async function (request: any, reply) {
+      return fastify.recipeService.findManyRecipes({
+        name: request.query.name,
+        sortColumn: request.queryy.sortColumn,
+        sortOrder: request.query.sortOrder,
+        take: request.query.take,
+        skip: request.query.skip,
+      })
+    },
+  )*/
---
+++ apps/recipestacker-api2/src/services/RecipeService.ts
@@ -0,0 +1,200 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+import { FastifyBaseLogger } from 'fastify'
+
+export enum SortOrder {
+  ASC = 'asc',
+  DESC = 'desc',
+}
+
+export const DEFAULT_TAKE = 15
+export const DEFAULT_SKIP = 0
+
+interface RecipeServiceProps {
+  logger: FastifyBaseLogger
+  prisma: PrismaClient
+}
+
+interface FindOneRecipeProps {
+  recipe_id: string
+}
+
+interface FindManyRecipeProps {
+  name?: string
+  sortColumn?: string
+  sortOrder?: SortOrder
+  take?: number
+  skip?: number
+}
+
+interface CreateIngredientMeasurementProps {
+  ingredient_id?: string
+  ingredient_name?: string
+  ingredient_description?: string
+  unit?: string
+  quantity?: number
+}
+
+interface UpdateOneRecipeProps {
+  recipe_id: string
+  name: string
+  description: string
+  ingredient_measurments: CreateIngredientMeasurementProps[]
+}
+
+interface CreateOneRecipeProps {
+  name: string
+  description: string
+  ingredient_measurments: CreateIngredientMeasurementProps[]
+}
+
+interface GetRecipeOrderByProps {
+  sortColumn: string
+  sortOrder: SortOrder
+}
+
+export class RecipeService {
+  logger: FastifyBaseLogger
+  prisma: PrismaClient
+
+  constructor({ logger, prisma }: RecipeServiceProps) {
+    this.logger = logger
+    this.prisma = prisma
+  }
+
+  getRecipeOrderBy({
+    sortOrder,
+    sortColumn,
+  }: GetRecipeOrderByProps): Prisma.RecipeOrderByWithRelationInput | Prisma.RecipeOrderByWithRelationInput[] {
+    return {
+      [sortColumn]: sortOrder,
+    }
+  }
+
+  async findOneRecipe(props: FindOneRecipeProps) {
+    this.logger.info({ props }, 'findOneRecipes')
+    const { recipe_id } = props
+    return this.prisma.recipe.findFirst({
+      where: {
+        recipe_id,
+      },
+      include: {
+        ingredient_measurments: {
+          include: {
+            ingredient: true,
+          },
+        },
+      },
+    })
+  }
+
+  async updateOneRecipe(props: UpdateOneRecipeProps) {
+    this.logger.info({ props }, 'updateOneRecipes')
+    // const user_id = ''
+    const spoof_user_id = '111'
+    const { ingredient_measurments, recipe_id, ...rest } = props
+    const updatedRecipe = await this.prisma.recipe.update({
+      where: {
+        recipe_id,
+      },
+      data: {
+        ...rest,
+        user: {
+          connect: { user_id: spoof_user_id },
+        },
+        ingredient_measurments: {
+          upsert: ingredient_measurments?.map(
+            ({ ingredient_id, ingredient_name, ingredient_description, quantity, unit }) => ({
+              where: {
+                ingredient_id_recipe_id: {
+                  ingredient_id: ingredient_id || '',
+                  recipe_id,
+                },
+              },
+              update: {
+                quantity,
+                unit,
+              },
+              create: {
+                ingredient: ingredient_id
+                  ? {
+                      connect: {
+                        ingredient_id,
+                      },
+                    }
+                  : {
+                      create: {
+                        name: ingredient_name,
+                        description: ingredient_description,
+                      },
+                    },
+                unit,
+                quantity,
+              },
+            }),
+          ),
+        },
+      },
+    })
+    return updatedRecipe
+  }
+
+  async findManyRecipes(props: FindManyRecipeProps) {
+    this.logger.info({ props }, 'findManyRecipes')
+    const { name, sortColumn = 'name', sortOrder = SortOrder.ASC, take = DEFAULT_TAKE, skip = DEFAULT_SKIP } = props
+    const orderBy = this.getRecipeOrderBy({ sortColumn, sortOrder })
+    return this.prisma.recipe.findMany({
+      where: {
+        name,
+      },
+      orderBy: {
+        name: SortOrder.ASC,
+      },
+      take,
+      skip,
+      include: {
+        ingredient_measurments: {
+          include: {
+            ingredient: true,
+          },
+        },
+      },
+    })
+  }
+
+  async createOneRecipe(props: CreateOneRecipeProps) {
+    const { name, description, ingredient_measurments } = props
+    const spoof_user_id = '111'
+    const recipe = await this.prisma.recipe.create({
+      data: {
+        user: {
+          connect: {
+            user_id: spoof_user_id,
+          },
+        },
+        name,
+        description,
+        ingredient_measurments: {
+          create: ingredient_measurments.map(
+            ({ ingredient_id, ingredient_description, ingredient_name, unit, quantity }) => ({
+              ingredient: ingredient_id
+                ? {
+                    connect: {
+                      ingredient_id,
+                    },
+                  }
+                : {
+                    create: {
+                      name: ingredient_name,
+                      description: ingredient_description,
+                    },
+                  },
+              quantity,
+              unit,
+            }),
+          ),
+        },
+      },
+    })
+    return recipe
+  }
+}
