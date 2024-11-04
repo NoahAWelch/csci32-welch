49cd9c7b216ede1b25fc5e0af2be15ab2d9e7298
noah welch <noahwelch13@yahoo.com>
2024-11-04 12:20:19 -0800

    recipe adding frontend

--- apps/csci32-nextjs-welch/src/app/Games/random-number-guesser/random-number-game-menu.tsx
+++ apps/csci32-nextjs-welch/src/app/Games/random-number-guesser/random-number-game-menu.tsx
@@ -27,12 +27,27 @@ export default function RandomNumberGameMenu({ startGame }: GuessingGameMenuProp
             <h1 className="text-2xl font-bold">please enter your min number guess</h1>
           </header>
           <form className="flex flex-col gap-4 bg-gray-500 py-2" onSubmit={onSubmitSettings}>
-            <Input defaultValue={0} type="number" placeholder={'Minimum guessing value'} name="minimum" id="minimum" />
+            <Input
+              defaultValue={0}
+              setValue
+              type="number"
+              placeholder={'Minimum guessing value'}
+              name="minimum"
+              id="minimum"
+            />
             <h2 className="text-2xl font-bold">and max number guess</h2>
-            <Input defaultValue={10} type="number" placeholder={'Maximum guessing value'} name="maximum" id="maximum" />
+            <Input
+              defaultValue={10}
+              setValue
+              type="number"
+              placeholder={'Maximum guessing value'}
+              name="maximum"
+              id="maximum"
+            />
             <h3 className="text-2xl font-bold">and enter the number of guesses you want to have.</h3>
             <Input
               defaultValue={3}
+              setValue
               type="number"
               placeholder={'All guesses consumed'}
               name="maxGuessCount"
